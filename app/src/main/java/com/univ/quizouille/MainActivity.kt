4c712d761ca2e18bf0b966bd59ffa01ad0768854
RODRIGUEZ lucas <lucasrodriguez@free.fr>
2023-12-13 10:08:34 +0100

    Remove unused function

--- app/src/main/java/com/univ/quizouille/MainActivity.kt
+++ app/src/main/java/com/univ/quizouille/MainActivity.kt
@@ -16,31 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            QuizouilleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Text(text = "Quizouille")
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuizouilleTheme {
-        Greeting("Android")
     }
 }
